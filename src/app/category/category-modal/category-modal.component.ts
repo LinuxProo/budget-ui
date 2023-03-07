3161c3384417870471bffed44c73d589f3ec774b
LinuxProo <julian.fiore@lernen.siw.swiss>
2023-03-07 21:57:54 +0100

    kategorien hinzugefügt (backendurl missing)

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -2,21 +2,51 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
 import { filter, from } from 'rxjs';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Category} from "../../shared/domain";
+import {CategoryService} from "../category.service";
+import {ToastService} from "../../shared/service/toast.service";
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
 export class CategoryModalComponent {
-  constructor(private readonly actionSheetService: ActionSheetService, private readonly modalCtrl: ModalController) {}
 
+constructor(
+    private readonly actionSheetService: ActionSheetService,
+    private readonly categoryService: CategoryService,
+    private readonly formBuilder: FormBuilder,
+    private readonly modalCtrl: ModalController,
+    private readonly toastService: ToastService
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(40)]],
+    });
+  }
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
   save(): void {
-    this.modalCtrl.dismiss(null, 'save');
+    this.submitting = true;
+    this.categoryService.upsertCategory(this.categoryForm.value).subscribe({
+      next: () => {
+        this.toastService.displaySuccessToast('Category saved');
+        this.modalCtrl.dismiss(null, 'refresh');
+        this.submitting = false;
+      },
+      error: (error) => {
+        this.toastService.displayErrorToast('Could not save category', error);
+        this.submitting = false;
+      },
+    });
   }
+  // Passed into the component by the ModalController, available in the ngOnInit
+  category: Category = {} as Category;
+
+  readonly categoryForm: FormGroup;
+  submitting = false;
 
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this category?'))
